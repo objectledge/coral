9cf247b1b1ef7048255985af19bb6497c1564688
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-02-20 12:25:02 +0000

    base entity implementations

---
+++ coral-ri/src/main/java/org/objectledge/coral/entity/AbstractAssignment.java
@@ -0,0 +1,146 @@
+package org.objectledge.coral.entity;
+
+import java.util.Date;
+
+import org.objectledge.coral.security.CoralSecurity;
+import org.objectledge.coral.security.Subject;
+import org.objectledge.database.persistence.InputRecord;
+import org.objectledge.database.persistence.OutputRecord;
+import org.objectledge.database.persistence.PersistenceException;
+
+/**
+ * Base of {@link Assignment} implementations.
+ *
+ * @version $Id: AbstractAssignment.java,v 1.1 2004-02-20 12:25:02 fil Exp $
+ * @author <a href="mailto:rkrzewsk@ngo.pl">Rafal Krzewski</a>
+ */
+public abstract class AbstractAssignment
+    extends AbstractAssociation
+    implements Assignment
+{
+    // Member objects ////////////////////////////////////////////////////////
+
+
+    /** CoralSecurity. */
+    protected CoralSecurity security;
+
+    /** The grantor */
+    protected Subject grantor;
+    
+    /** Grant time*/
+    protected Date grantTime;
+
+    // Initialization ////////////////////////////////////////////////////////
+
+    /**
+     * Constructs an assignment.
+     * 
+     * @param security the CoralSecurity subsystem.
+     */
+    public AbstractAssignment(CoralSecurity security) 
+    {
+        this.security = security;
+    }
+
+    /**
+     * Constructs an assignment object.
+     *
+     * @param security the CoralSecurity subsystem.
+     * @param grantor the grantor.
+     * @param grantTime grant time.
+     */
+    public AbstractAssignment(CoralSecurity security, Subject grantor, Date grantTime)
+    {
+        this.grantor = grantor;
+        this.grantTime = grantTime;
+    }
+
+    // Persistent interface //////////////////////////////////////////////////
+
+    /**
+     * Stores the fields of the object into the specified record.
+     *
+     * <p>You need to call <code>getData</code> of your superclasses if they
+     * are <code>Persistent</code>.</p>
+     *
+     * @param record the record to store state into.
+     * @throws PersistenceException if there is a problem storing field values.
+     */
+    public void getData(OutputRecord record)
+        throws PersistenceException
+    {
+        record.setLong("grantor", grantor.getId());
+        record.setDate("grant_time", grantTime);
+    }
+
+    /**
+     * Loads the fields of the object from the specified record.
+     *
+     * <p>You need to call <code>setData</code> of your superclasses if they
+     * are <code>Persistent</code>.</p>
+     * 
+     * @param record the record to read state from.
+     * @throws PersistenceException if there is a problem loading field values.
+     */
+    public void setData(InputRecord record)
+        throws PersistenceException
+    {
+        long grantorId = record.getLong("grantor");
+        try
+        {
+            grantor = security.getSubject(grantorId);
+        }
+        catch(EntityDoesNotExistException e)
+        {
+            String type = getClass().getName();
+            int pos = type.lastIndexOf(".");
+            type = type.substring(pos+1);
+            throw new PersistenceException("Failed to load "+type, e);
+        }
+        grantTime = record.getDate("grant_time");
+    }
+
+    // Assignment interface /////////////////////////////////////////////////
+    
+    /**
+     * Returns the {@link Subject} that created this assignment.
+     *
+     * @return the {@link Subject} that created this assignment.
+     */
+    public Subject getGrantedBy()
+    {
+        return grantor;
+    }
+
+    /**
+     * Returns the time the assignement was created.
+     *
+     * @return the time the assignement was created.
+     */
+    public Date getGrantTime()
+    {
+        return grantTime;
+    }
+    
+    // implementation specific ///////////////////////////////////////////////
+
+    /**
+     * Sets the {@link Subject} that created this assignment.
+     *
+     * @param grantor the {@link Subject} that created this assignment.
+     */
+    public void setGrantedBy(Subject grantor)
+    {
+        this.grantor = grantor;
+    }
+
+    /**
+     * Sets the time the assignement was created.
+     *
+     * @param grantTime the time the assignement was created.
+     */
+    public void setGrantTime(Date grantTime)
+    {
+        this.grantTime = grantTime;
+    }
+}
---
+++ coral-ri/src/main/java/org/objectledge/coral/entity/AbstractEntity.java
@@ -0,0 +1,214 @@
+package org.objectledge.coral.entity;
+
+import org.objectledge.database.persistence.InputRecord;
+import org.objectledge.database.persistence.OutputRecord;
+import org.objectledge.database.persistence.Persistence;
+import org.objectledge.database.persistence.PersistenceException;
+import org.objectledge.database.persistence.PersistentObject;
+
+/**
+ * A base of all ARL entitity implementations.
+ *
+ * @version $Id: AbstractEntity.java,v 1.1 2004-02-20 12:25:02 fil Exp $
+ * @author <a href="mailto:rkrzewsk@ngo.pl">Rafal Krzewski</a>
+ */
+public abstract class AbstractEntity
+    extends PersistentObject
+    implements Entity
+{
+    // Member objects ////////////////////////////////////////////////////////
+
+    /** The persistence system. */
+    protected Persistence persistence;
+
+    /** The numerical identifier of the entity. */
+    protected long id;
+    
+    /** The name of the entity. */
+    protected String name;
+
+    // Initialization ////////////////////////////////////////////////////////
+    
+    /**
+     * Constructs an entity
+     *
+     * @param persistence the persistence system.
+     * @param name the name of the entity.
+     */
+    public AbstractEntity(Persistence persistence, String name)
+    {
+        this.persistence = persistence;
+        this.id = -1L;
+        this.name = name;
+    }
+
+    /**
+     * Constructs an entity
+     *
+     * @param persistence the persistence system.
+     */
+    public AbstractEntity(Persistence persistence)
+    {
+        this.persistence = persistence;
+        this.id = -1L;
+        this.name = null;
+    }
+
+    // Hashing & equality ////////////////////////////////////////////////////
+
+    /**
+     * Returs the hashcode for this entity.
+     *
+     * <p>The hashcode if computed as a XOR of the entity's actual class
+     * hashcode and the identifier multiplied by 0x11111111.</p>
+     *
+     * @return the hashcode of the object.
+     */
+    public int hashCode()
+    {
+        return getClass().hashCode() ^ (int)(id * 0x11111111);
+    }
+
+    /**
+     * Checks if another object represens the same entity.
+     *
+     * <p>The check involves the actual class of the object and the value of the
+     * indetifier.</p>
+     *
+     * @param obj the other objects.
+     * @return <code>true</code> if the other object represents the same entity.
+     */
+    public boolean equals(Object obj)
+    {
+        if(obj != null && obj.getClass().equals(getClass()))
+        {
+            return id == ((Entity)obj).getId();
+        }
+        return false;
+    }    
+
+    /**
+     * Returns a String representation of this object.
+     *
+     * <p> This method is overriden to augument debugging. Not only the
+     * semantical hashcode is inculded in the represtentation, but also the
+     * object instance's identity hashcode, as returned by
+     * <code>System.getIdentityHashCode(Object)</code>. This is useful for
+     * debugging data consistency issues. </p>
+     * 
+     * @return a String representation of this object.
+     */
+    public String toString()
+    {
+        StringBuffer buff = new StringBuffer();
+        buff.append(getClass().getName());
+        buff.append('@');
+        buff.append(Integer.toString(System.identityHashCode(this), 16));
+        buff.append(':');
+        buff.append(Integer.toString(hashCode(), 16));
+        return buff.toString();
+    }
+
+    // Persistent interface //////////////////////////////////////////////////
+
+    /** 
+     * Returns the name of the primary key column.
+     *
+     * @return the name of the primary key column.
+     */
+    public abstract String[] getKeyColumns();
+
+    /**
+     * Stores the fields of the object into the specified record.
+     *
+     * <p>You need to call <code>getData</code> of your superclasses if they
+     * are <code>Persistent</code>.</p>
+     *
+     * @param record the record to store state into.
+     * @throws PersistenceException
+     * @throws PersistenceException if there is a problem storing field values.
+     */
+    public void getData(OutputRecord record)
+        throws PersistenceException
+    {
+        record.setLong(getKeyColumns()[0], id);
+        record.setString("name", name);
+    }
+    
+    /**
+     * Loads the fields of the object from the specified record.
+     *
+     * <p>You need to call <code>setData</code> of your superclasses if they
+     * are <code>Persistent</code>.</p>
+     * 
+     * @param record the record to read state from.
+     * @throws PersistenceException if there is a problem loading field values.
+     */
+    public void setData(InputRecord record)
+        throws PersistenceException
+    {
+        id = record.getLong(getKeyColumns()[0]);
+        name = record.getString("name");
+    }
+
+    /**
+     * Sets the 'saved' flag for the object.
+     *
+     * <p>The id generation will take place only for objects that declare a
+     * single column primary key. Othre objects will receive a <code>-1</code>
+     * as the <code>id</code> parameter. After this call is made on an object,
+     * subsequent calls to {@link #getSaved()} on the same object should
+     * return true.</p> 
+     *
+     * @param id The generated value of the primary key.
+     */
+    public void setSaved(long id)
+    {
+        this.saved = true;
+        this.id = id;
+    }
+
+    // Entity inteface ///////////////////////////////////////////////////////
+    
+    /**
+     * Returns the numerical identifier of the entity.
+     * 
+     * @return the numerical identifier of the entity.
+     */
+    public long getId()
+    {
+        return id;
+    }
+    
+    /**
+     * Returns the name of the entity.
+     *
+     * @return the name of the entity.
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    // Package private setter methods ////////////////////////////////////////
+
+    /**
+     * Sets the numerical identifier of the entity.
+     * 
+     * @param id the numerical identifier of the entity.
+     */
+    void setId(long id)
+    {
+        this.id = id;
+    }
+    
+    /**
+     * Sets the name of the entity.
+     *
+     * @param name the name of the entity.
+     */
+    void setName(String name)
+    {
+        this.name = name;
+    }
+}
