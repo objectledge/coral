bfa1fc4c1291293e83041592d93883a0b01a9e60
Damian Gajda <damian.gajda@caltha.pl>
2004-03-01 13:54:10 +0000

    Moved the parser test into test source directory

--- coral-api/src/test/org/objectledge/coral/relation/query/parser/RelationQueryParserTest.java
+++ coral-api/src/test/org/objectledge/coral/relation/query/parser/RelationQueryParserTest.java
@@ -31,7 +31,7 @@ import junit.framework.TestCase;
 
 /**
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: RelationQueryParserTest.java,v 1.1 2004-03-01 13:52:26 zwierzem Exp $
+ * @version $Id: RelationQueryParserTest.java,v 1.1 2004-03-01 13:54:10 zwierzem Exp $
  */
 public class RelationQueryParserTest extends TestCase
 {
