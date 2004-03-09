45edcad576e1aca61cb67c77f67324860b007d78
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-03-09 09:34:35 +0000

    unit test for coral session impl

---
+++ coral-ri/src/test/java/org/objectledge/coral/session/CoralSessionImplTest.java
@@ -0,0 +1,148 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.coral.session;
+
+import java.security.Principal;
+
+import org.apache.commons.pool.KeyedObjectPool;
+import org.jmock.builder.Mock;
+import org.jmock.builder.MockObjectTestCase;
+import org.objectledge.coral.CoralCore;
+import org.objectledge.coral.security.Subject;
+
+/**
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: CoralSessionImplTest.java,v 1.1 2004-03-09 09:34:35 fil Exp $
+ */
+public class CoralSessionImplTest extends MockObjectTestCase
+{
+    private Mock mockCoralCore;
+    private CoralCore coralCore;
+    private Mock mockKeyedObjectPool;
+    private KeyedObjectPool keyedObjectPool;
+    
+    private CoralSessionImpl coralSession;
+    
+    private Mock mockSubject;
+    private Subject subject;
+    private Mock mockPrincipal;
+    private Principal principal;
+    
+    public void setUp()
+    {
+        mockCoralCore = new Mock(CoralCore.class);
+        coralCore = (CoralCore)mockCoralCore.proxy();
+        mockKeyedObjectPool = new Mock(KeyedObjectPool.class);
+        keyedObjectPool = (KeyedObjectPool)mockKeyedObjectPool.proxy();
+        
+        coralSession = new CoralSessionImpl(coralCore, keyedObjectPool);
+        
+        mockSubject = new Mock(Subject.class);
+        subject = (Subject)mockSubject.proxy();
+        mockPrincipal = new Mock(Principal.class);
+        principal = (Principal)mockPrincipal.proxy();
+    }
+    
+    public void testBasicLifecycle()
+    {
+        coralSession.open(principal, subject);
+        
+        mockKeyedObjectPool.expect(once()).method("returnObject").with(same(coralSession), same(principal)).isVoid();
+        assertEquals(principal, coralSession.getUserPrincipal());
+        assertEquals(subject, coralSession.getUserSubject());
+
+        coralSession.getEvent();
+        
+        coralSession.getQuery();
+        
+        coralSession.getSchema();
+        
+        coralSession.getSecurity();
+        
+        coralSession.getStore();
+
+        coralSession.close();
+        
+        try
+        {
+            coralSession.close();
+            fail("exception expected");
+        }
+        catch(Exception e)
+        {
+            assertEquals(IllegalStateException.class, e.getClass());
+        }
+    }
+    
+    public void testCrossThreadMischief()
+    {
+        coralSession.open(principal, subject);
+        mockKeyedObjectPool.expect(once()).method("returnObject").with(same(coralSession), same(principal)).isVoid();
+        coralSession.close();
+        // session is closed and returned to the pool
+        
+        final Object semaphore = new Object();
+        Runnable runnable = new Runnable() {
+            public void run()
+            {
+                // another thread opens session, it gets pre-owned session instance
+                coralSession.open(principal, subject);
+                synchronized(semaphore)
+                {
+                    semaphore.notify();
+                }
+            }
+        };
+        Thread otherThread = new Thread(runnable);
+        // make sure the other thread does not attempt to wake us up before we actually go to sleep
+        synchronized(semaphore)
+        {
+            otherThread.start();
+            try
+            {
+                semaphore.wait();
+            }
+            catch(InterruptedException e)
+            {               
+            }
+        }
+        // do evil - attempt to run an operation on a stale session reference that was possibly
+        // reopened by another thread, with other user's credentials
+        try
+        {
+            coralSession.getStore().deleteResource(null);
+            fail("exception expected");
+        }
+        catch(Exception e)
+        {
+            assertEquals(IllegalStateException.class, e.getClass());
+            assertEquals("attempted to use session from wrong thread", e.getMessage());
+        }
+    }
+}
