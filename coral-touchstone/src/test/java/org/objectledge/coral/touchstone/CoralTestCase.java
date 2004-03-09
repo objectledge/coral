559bdf2098a456f585a44d0e83d6c8ce357138ec
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-03-09 14:45:50 +0000

    initial import

---
+++ coral-touchstone/src/test/java/org/objectledge/coral/touchstone/CoralTestCase.java
@@ -0,0 +1,63 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.coral.touchstone;
+
+import junit.framework.TestCase;
+
+import org.objectledge.container.LedgeContainer;
+import org.objectledge.coral.CoralSessionFactory;
+import org.objectledge.filesystem.FileSystem;
+
+/**
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: CoralTestCase.java,v 1.1 2004-03-09 14:45:50 fil Exp $
+ */
+public abstract class CoralTestCase extends TestCase
+{
+    protected CoralSessionFactory coralSessionFactory;
+    
+    protected LedgeContainer container;
+    
+    private long t;
+    
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+        FileSystem fs = FileSystem.getStandardFileSystem("src/test/resources");
+        container = new LedgeContainer(fs, "/config", getClass().getClassLoader()); 
+        coralSessionFactory = (CoralSessionFactory)container.getContainer().
+            getComponentInstance(CoralSessionFactory.class);   
+    }
+    
+    public void tearDown()
+    {
+        container.killContainer();
+    }
+}
