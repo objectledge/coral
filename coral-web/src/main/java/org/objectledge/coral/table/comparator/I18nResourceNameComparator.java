b0da9a9b4a77ad0348e26b9818168f450bcfaf63
Damian Gajda <damian.gajda@caltha.pl>
2004-08-19 16:15:35 +0000

    NUCYKLO-33 Added basic name caching. Still need to fix name getting.

--- coral-web/src/main/java/org/objectledge/coral/table/comparator/I18nResourceNameComparator.java
+++ coral-web/src/main/java/org/objectledge/coral/table/comparator/I18nResourceNameComparator.java
@@ -1,6 +1,8 @@
 package org.objectledge.coral.table.comparator;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import org.objectledge.coral.i18n.CoralI18nTool;
 import org.objectledge.coral.store.Resource;
@@ -10,11 +12,12 @@ import org.objectledge.i18n.I18n;
  * This is a comparator for comparing localized coral resource names.
  *
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: I18nResourceNameComparator.java,v 1.2 2004-08-19 13:40:23 zwierzem Exp $
+ * @version $Id: I18nResourceNameComparator.java,v 1.3 2004-08-19 16:15:35 zwierzem Exp $
  */
 public class I18nResourceNameComparator
     extends NameComparator
 {
+    private Map nameCache = new HashMap();
     private I18n i18n;
     private Locale locale;
     
@@ -27,16 +30,28 @@ public class I18nResourceNameComparator
     
     public int compare(Object o1, Object o2)
     {
-        if(!((o1 instanceof Resource && o2 instanceof Resource )))
-        {
-            return 0;
-        }
-
-        Resource r1 = (Resource)o1;
-        Resource r2 = (Resource)o2;
+        String name1 = getName(o1);
+        String name2 = getName(o2);
         
-        return compareStrings(
-            i18n.get(locale, CoralI18nTool.getNameKey(r1), r1.getName()),
-            i18n.get(locale, CoralI18nTool.getNameKey(r2), r2.getName()));
+        return compareStrings(name1, name2);
+    }
+    
+    private String getName(Object o)
+    {
+        String name = (String) nameCache.get(o);
+        if(name == null)
+        {
+            if(o instanceof Resource)
+            {
+                Resource r = (Resource) o;
+                name = i18n.get(locale, CoralI18nTool.getNameKey(r), r.getName());
+            }
+            else
+            {
+                name = "";
+            }
+            nameCache.put(o, name);
+        }
+        return name;
     }
 }
