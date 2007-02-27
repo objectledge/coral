0bc4a6486d2bd9052e7a61693220f81f345b859a
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-02-27 21:40:41 +0000

    Improved functionality for working with Subject lists - sorting & filtering according to personal data properties (uid, surname, mail etc.)

--- coral-web/src/main/java/org/objectledge/coral/table/SubjectListTableModel.java
+++ coral-web/src/main/java/org/objectledge/coral/table/SubjectListTableModel.java
@@ -1,14 +1,17 @@
 package org.objectledge.coral.table;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.objectledge.authentication.UserManager;
 import org.objectledge.coral.security.Subject;
 import org.objectledge.coral.table.comparator.IdComparator;
 import org.objectledge.coral.table.comparator.NameComparator;
+import org.objectledge.coral.table.comparator.PersonalDataComparator;
 import org.objectledge.table.TableColumn;
 import org.objectledge.table.TableException;
 import org.objectledge.table.generic.ListTableModel;
@@ -17,7 +20,7 @@ import org.objectledge.table.generic.ListTableModel;
  * Implementation of Table Model for lists of ARL resources.
  *
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: SubjectListTableModel.java,v 1.5 2005-02-21 14:04:39 rafal Exp $
+ * @version $Id: SubjectListTableModel.java,v 1.6 2007-02-27 21:40:41 rafal Exp $
  */
 public class SubjectListTableModel extends ListTableModel
 {
@@ -29,13 +32,15 @@ public class SubjectListTableModel extends ListTableModel
      * 
      * @param array an array of Subjects.
      * @param locale the locale to be used by comparators.
+     * @param properties the names of personal data properties to use.
+     * @param userManager the user manager component, may be null if properties is empty.
      * @throws TableException if there is a problem creating the model.
      */
-    public SubjectListTableModel(Subject[] array, Locale locale)
+    public SubjectListTableModel(Subject[] array, Locale locale, String[] properties, UserManager userManager)
         throws TableException
     {
         super(array, null);
-        columns = getColumns(locale);
+        columns = getColumns(locale, properties, userManager);
     }
         
     /**
@@ -43,30 +48,38 @@ public class SubjectListTableModel extends ListTableModel
      * 
      * @param list a list of Subjects.
      * @param locale the locale to be used by comparators.
+     * @param properties the names of personal data properties to use.
+     * @param userManager the user manager component, may be null if properties is empty.
      * @throws TableException if there is a problem creating the model.
      */
-    public SubjectListTableModel(List list, Locale locale)
+    public SubjectListTableModel(List list, Locale locale, String[] properties, UserManager userManager)
         throws TableException
     {
         super(list, null);
-        columns = getColumns(locale);
+        columns = getColumns(locale, properties, userManager);
     }
 
     /**
      * Return model columns.
      * 
      * @param locale the locale to be used by comparators.
+     * @param properties TODO
+     * @param userManager TODO
      * @return an array of column objects.
      * @throws TableException if there is a problem creating column objects.
      */
-    protected TableColumn[] getColumns(Locale locale)
+    protected TableColumn[] getColumns(Locale locale, String[] properties, UserManager userManager)
         throws TableException
     {
-        TableColumn[] columns = new TableColumn[2];
-        // Name comparator
-        columns[0] = new TableColumn("name", new NameComparator(locale));
-        // Id comparator
+        TableColumn[] columns = new TableColumn[2 + properties.length];
+        columns[0] = new TableColumn("dn", new NameComparator(locale));
         columns[1] = new TableColumn("id", new IdComparator());
+        int i = 2;
+        for(String property : properties)
+        {
+            Comparator comparator = new PersonalDataComparator(userManager, property, locale);
+            columns[i++] = new TableColumn(property, comparator);
+        }
         return columns;
     }
 
