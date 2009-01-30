059322749c7a309e870add8915fe7a4725203bc1
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-01-30 13:42:14 +0000

    CYKLO-545:  Some handy methods for manipulating Entities

---
+++ coral-api/src/main/java/org/objectledge/coral/entity/EntityUtils.java
@@ -0,0 +1,92 @@
+package org.objectledge.coral.entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Collection of helper methods for manipulating Entities.
+ * 
+ * @author rafal
+ */
+public class EntityUtils
+{
+    /**
+     * A private constructor to prevent instantiation.
+     */
+    private EntityUtils()
+    {
+    }
+
+    /**
+     * Converts a collection of entities into a comma separated string of entity ids.
+     * <p>
+     * Note that information about Entity class is not included in the string.
+     * </p>
+     * 
+     * @param <E> Entity subclass.
+     * @param entities a Collection of Entities.
+     * @return comma separated String of Entity ids.
+     */
+    public static <E extends Entity>String entitiesToIds(Collection<E> entities)
+    {
+        StringBuilder sb = new StringBuilder();
+        Iterator<E> i = entities.iterator();
+        while(i.hasNext())
+        {
+            sb.append(i.next().getIdString());
+            if(i.hasNext())
+            {
+                sb.append(",");
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Converts a comma separated string of Entity ids into a list of Entities using specified factory.
+     * 
+     * @param <E> Entity subclass.
+     * @param idString comma separated string of Entity id's
+     * @param factory an Entity factory.
+     * @return a list of Entities.
+     * @throws NumberFormatException if the string contains numbers that cannot be parsed correctly
+     * @throws EntityDoesNotExistException if the factory is unable to provide Entities with specified ids
+     */
+    public static <E extends Entity>List<E> idsToEntityList(String idString, EntityFactory<E> factory)
+        throws NumberFormatException, EntityDoesNotExistException
+    {
+        String[] ids = idString.split(",");
+        List<E> entities = new ArrayList<E>(ids.length);
+        for (String id : ids)
+        {
+            entities.add(factory.getEntity(Long.parseLong(id)));
+        }
+        return entities;
+    }
+
+    /**
+     * Converts a comma separated string of Entity ids into a set of Entities using specified factory.
+     * 
+     * @param <E> Entity subclass.
+     * @param idString comma separated string of Entity id's
+     * @param factory an Entity factory.
+     * @return a set of Entities.
+     * @throws NumberFormatException if the string contains numbers that cannot be parsed correctly
+     * @throws EntityDoesNotExistException if the factory is unable to provide Entities with specified ids
+     */
+    public static <E extends Entity>Set<E> idsToEntitySet(String idString, EntityFactory<E> factory)
+        throws NumberFormatException, EntityDoesNotExistException
+    {
+        String[] ids = idString.split(",");
+        Set<E> entities = new HashSet<E>(ids.length);
+        for (String id : ids)
+        {
+            entities.add(factory.getEntity(Long.parseLong(id)));
+        }
+        return entities;
+    }
+}
