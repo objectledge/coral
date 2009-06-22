5d02314c0232fdaf058e3cb1430fc7fda919c683
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-06-22 14:14:03 +0000

    retagging release 1.0.3-b3 for last minute changes

---
+++ coral-api/src/main/java/org/objectledge/coral/schema/ResourceHandler.java
@@ -0,0 +1,211 @@
+package org.objectledge.coral.schema;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.objectledge.coral.store.Resource;
+import org.objectledge.coral.store.ValueRequiredException;
+
+/**
+ * Manages persistency of resources belonging to a specific class .
+ *
+ * @version $Id: ResourceHandler.java,v 1.8 2005-10-23 10:11:44 rafal Exp $
+ * @author <a href="mailto:rkrzewsk@ngo.pl">Rafal Krzewski</a>
+ */
+public interface ResourceHandler
+{
+    // NOTE ResoureClass needs to be passed to the constructor.
+
+    /**
+     * Saves a new resource in the persistent storage.
+     *
+     * @param delegate the security delegate {@link Resource} object.
+     * @param attributes the initial values of the attributes, keyed with
+     *        {@link AttributeDefinition} objects.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @return the new resource.
+     * @throws ValueRequiredException if not all obligatory attributes are
+     *         provided.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     */
+    public Resource create(Resource delegate, Map attributes, Connection conn)
+        throws ValueRequiredException, SQLException;
+
+    /**
+     * Removes the resource from the persistent storage.
+     *
+     * @param resource the resource.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     */
+    public void delete(Resource resource, Connection conn)
+        throws SQLException;
+
+    /**
+     * Retrives a resource from the persistent storage.
+     *
+     * @param delegate the security delegate {@link Resource} object.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @param data implemntation dependent data passed across recursive calls to this method. 
+     *        An implementation must accept <code>null</code> value of this parameter.  
+     * @return the resource object.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     */
+    public Resource retrieve(Resource delegate, Connection conn, Object data)
+        throws SQLException;
+
+
+    /**
+     * Reverts the state of a resource from the persistent storage.
+     *
+     * @param resource the resource.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @param data implemntation dependent data passed across recursive calls to this method. 
+     *        An implementation must accept <code>null</code> value of this parameter.  
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     */
+    public void revert(Resource resource, Connection conn, Object data)
+        throws SQLException;
+    
+    /**
+     * Reverts the state of all resources of the specified class from the persistent storage.
+     * 
+     * @param resourceClass the resource class.
+     * @param conn the JDBC connection.
+     * @throws SQLException in case of database problems.
+     */
+    public void revert(ResourceClass resourceClass, Connection conn)
+        throws SQLException;
+
+    /**
+     * Updates the contents of the resource in the persistent storage.
+     *
+     * @param resource the resource.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     */
+    public void update(Resource resource, Connection conn)
+        throws SQLException;
+
+    /**
+     * Called when an attribute is added to a structured resource class.
+     *
+     * <p>Concrete resource classes will probably deny this operation by
+     * throwing <code>UnsupportedOperationException</code>.</p>
+     *
+     * @param attribute the new attribute.
+     * @param value the initial value to be set for existing instances of this
+     *        class.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     * @throws ValueRequiredException if <code>null</code> value was provided
+     *         for a REQUIRED attribute.
+     */
+    public void addAttribute(AttributeDefinition attribute, Object value, 
+                             Connection conn)
+        throws ValueRequiredException, SQLException;
+    
+    /**
+     * Called when an attribute is removed from a structured resource class.
+     *
+     * <p>Concrete resource classes will probably deny this operation by
+     * throwing <code>UnsupportedOperationException</code>.</p>
+     *
+     * @param attribute the removed attribute.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     */
+    public void deleteAttribute(AttributeDefinition attribute, Connection conn)
+        throws SQLException;
+
+    /**
+     * Called when a parent class is added to a sturctured resource class.
+     *
+     * <p>Concrete resource classes will probably deny this operation by
+     * throwing <code>UnsupportedOperationException</code>.</p>
+     *
+     * @param parent the new parent class.
+     * @param attributes the initial values of the attributes. Values are
+     *        keyed with {@link AttributeDefinition} objects.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     * @throws ValueRequiredException if values for any of parent class
+     *         REQUIRED attributes are missing from <code>attributes</code>
+     *         map. 
+     */
+    public void addParentClass(ResourceClass parent, Map attributes, Connection conn)
+        throws ValueRequiredException, SQLException;
+    
+    /**
+     * Called when a parent class is removed from a structured resource class.
+     *
+     * <p>Concrete resource classes will probably deny this operation by
+     * throwing <code>UnsupportedOperationException</code>.</p>
+     *
+     * @param parent the new parent class.
+     * @param conn the JDBC <code>Connection</code> to use. Needed to perform
+     *        the operation as a part of a JDBC transaction.
+     * @throws SQLException in case of database problems. The caller metod
+     *         should consider rolling back the whole transaction.
+     */
+    public void deleteParentClass(ResourceClass parent, Connection conn)
+        throws SQLException;
+        
+     // integrity constraints ///////////////////////////////////////////////
+     
+    /**
+     * Returns all resources referenced by this resource through it's 
+     * attributes.
+     * 
+     * <p>The references mad throgh non-REQUIRED nor READONLY refernces
+     * may be auto-cleared by the handler with 
+     * {@link #clearResourceReferences(Resource)} method. These references
+     * may or may not be included in the result, depending on the clearable 
+     * parameter.</p> 
+     * 
+     * @param resource the resource.
+     * @param clearable references that may be auto-cleared are included in 
+     *        the results if <code>true</code>
+     * @return resources referenced by this resource.
+     */
+    public Resource[] getResourceReferences(Resource resource, boolean clearable);
+    
+    /**
+     * Removes all resource references that can be auto-cleared 
+     * from this resource.
+     * 
+     * <p>This method may be called during deletion of a group of 
+     * interdependant resources.</p>
+     * 
+     * @param resource the resource to clear.
+     */
+    public void clearResourceReferences(Resource resource);
+    
+    /**
+     * Retruns implementation dependent data that can speed up retrieving of all resources in the
+     * system.
+     * 
+     * @param conn database connection.
+     * @return implementation specific data.
+     * @throws SQLException if database operation fails.
+     */
+    public Object getData(Connection conn)
+        throws SQLException;
+}
---
+++ coral-api/src/main/java/org/objectledge/coral/schema/SchemaIntegrityException.java
@@ -0,0 +1,140 @@
+package org.objectledge.coral.schema;
+
+
+/**
+ * Thrown to indicate various schema integrity problems caused by adding
+ * attributes, parent resource classes, and attribute flags modifications.
+ *
+ * @version $Id: SchemaIntegrityException.java,v 1.4 2005-05-25 07:55:47 pablo Exp $
+ * @author <a href="mailto:rkrzewsk@ngo.pl">Rafal Krzewski</a>
+ */
+public class SchemaIntegrityException
+    extends Exception
+{
+	public static final long serialVersionUID = 0L;
+	
+    /** Value returned by {@link #getType()} for exceptions of unknown
+     *  origin. */
+    public static final int UNKNOWN = 0;
+
+    /** Value returned by {@link #getType()} for attribute class clash
+     *  exceptions. */
+    public static final int CLASS_CLASH = 1;
+    
+    /** Value returned by {@link #getType()} for attribute flags clash
+     *  exceptions. */
+    public static final int FLAGS_CLASH = 2;
+    
+    /** Value returned by {@link #getType()} for exceptions caused by
+     *  modification of flags of multiply-inherited attributes.  */ 
+    public static final int MULTIPLE_INHERITANCE = 4;
+
+    /**
+     * Returns the type of the inconsistency introduced by the requested
+     * operation. 
+     *
+     * @return one of {@link #CLASS_CLASH}, {@link #FLAGS_CLASH}, {@link
+     * #MULTIPLE_INHERITANCE} 
+     */
+    public int getType()
+    {
+        return type;
+    }
+
+    /**
+     * Returns the definition of the attribute that is causes the
+     * inconsistency. 
+     *
+     * @return the offending attribute.
+     */
+    public AttributeDefinition getAttribute()
+    {
+        return attribute;
+    }
+    
+    /**
+     * Returns the resource class that has the conflicting attribute.
+     *
+     * @return the offending class.
+     */
+    public ResourceClass getConflictingClass()
+    {
+        return resourceClass;
+    }
+
+    /**
+     * Returns <code>true</code> if the class returned by {@link
+     * #getConflictingClass()} is the superclass of the class on which the
+     * operation was performed.
+     *
+     * @return <code>true</code> if the class returned by {@link
+     * #getConflictingClass()} is the superclass of the class on which the
+     * operation was performed.
+     */
+    public boolean isSuperClass()
+    {
+        return superClass;
+    }
+    
+    /**
+     * Returns the other class involved in the conflict for multiple
+     * inheritance exceptions.
+     *
+     * @return the other involved class.
+     */
+    public ResourceClass getOtherClass()
+    {
+        return otherClass;
+    }
+
+    /** The exception type. */
+    private int type = UNKNOWN;
+
+    /** The involved attribute. */
+    private AttributeDefinition attribute = null;
+    
+    /** The involved resource class. */
+    private ResourceClass resourceClass = null;
+
+    /** Is resourceClass a super class of the class on which the opertaion was
+     *  performed? */
+    private boolean superClass = false;
+    
+    /** The other involved class. */
+    private ResourceClass otherClass = null;
+
+    /** 
+     * Construcs a new exception object with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public SchemaIntegrityException(String msg)
+    {
+        super(msg);
+    }
+    
+    /** 
+     * Construcs a new exception object with the specified detail message
+     * and background information.
+     *
+     * @param msg the detail message.
+     * @param type the exception type.
+     * @param attribute the involved attribute.
+     * @param resourceClass the involved resource class.
+     * @param superClass is the <code>resourceClass</code> a super class of
+     *                   the ``current'' class?
+     * @param otherClass the other involved resource class.
+     */
+    public SchemaIntegrityException(String msg, int type, 
+                                   AttributeDefinition attribute, 
+                                   ResourceClass resourceClass, 
+                                   boolean superClass,
+                                   ResourceClass otherClass)
+    {
+        super(msg);
+        this.type = type;
+        this.attribute = attribute;
+        this.resourceClass = resourceClass;
+        this.otherClass = otherClass;
+    }
+}
