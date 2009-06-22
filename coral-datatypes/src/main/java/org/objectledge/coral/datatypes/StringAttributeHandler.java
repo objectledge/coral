5c652ab6305bf78ac1ae87dc1deb175657f7bf0e
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-06-22 11:30:17 +0000

    release 1.0.3-b3

---
+++ coral-datatypes/src/main/java/org/objectledge/coral/datatypes/StringAttributeHandler.java
@@ -0,0 +1,185 @@
+package org.objectledge.coral.datatypes;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.schema.AttributeClass;
+import org.objectledge.coral.schema.CoralSchema;
+import org.objectledge.coral.security.CoralSecurity;
+import org.objectledge.coral.store.CoralStore;
+import org.objectledge.database.Database;
+
+/**
+ * Handles persistency of <code>java.lang.String</code> objects that contain
+ * at most 255 characters.
+ *
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: StringAttributeHandler.java,v 1.5 2005-01-20 10:48:26 rafal Exp $
+ */
+public class StringAttributeHandler
+    extends AttributeHandlerBase
+{
+    /** preloading cache. */
+    private String[] cache;
+
+    /**
+     * The constructor.
+     * 
+     * @param database the database.
+     * @param coralStore the store.
+     * @param coralSecurity the security.
+     * @param coralSchema the scheam.
+     * @param attributeClass the attribute class.
+     */
+    public StringAttributeHandler(Database database, CoralStore coralStore,
+                                  CoralSecurity coralSecurity, CoralSchema coralSchema,
+                                  AttributeClass attributeClass)
+    {
+        super(database, coralStore, coralSecurity, coralSchema, attributeClass);
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void preload(Connection conn)
+        throws SQLException
+    {
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT max(data_key) from "+getTable());
+        rs.next();
+        int count = rs.getInt(1);
+        cache = new String[count+1];
+        rs = stmt.executeQuery("SELECT data_key, data from "+getTable());
+        while(rs.next())
+        {
+            cache[rs.getInt(1)] = unescape(rs.getString(2));
+        }
+    }    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public long create(Object value, Connection conn)
+        throws SQLException
+    {
+        String str = (String)value;
+        if(str.length() > 255)
+        {
+            throw new IllegalArgumentException("maximum lenght of string attributes "+
+                                               "is 255 characters. Use text attributes "+
+                                               "wherever greater capacity is desired");
+        }
+
+        long id = getNextId();
+        Statement stmt = conn.createStatement();
+        stmt.execute(
+            "INSERT INTO "+getTable()+"(data_key, data) VALUES ("+
+            id+", '"+escape(str)+"')"
+        );
+        return id;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object retrieve(long id, Connection conn)
+        throws EntityDoesNotExistException, SQLException
+    {
+        if(cache != null && id < cache.length)
+        {
+            String value = cache[(int)id];
+            if(value != null)
+            {
+                return value;
+            }
+        }
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(
+            "SELECT data FROM "+getTable()+" WHERE data_key = "+id
+        );
+        if(!rs.next())
+        {
+            throw new EntityDoesNotExistException("Item #"+id+" does not exist in table "+
+                getTable());
+        }
+        String value = unescape(rs.getString(1));
+        if(cache != null && id < cache.length)
+        {
+            cache[(int)id] = value;
+        }
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void update(long id, Object value, Connection conn)
+        throws EntityDoesNotExistException, SQLException
+    {
+        String str = (String)value;
+        if(str.length() > 255)
+        {
+            throw new IllegalArgumentException("maximum lenght of string attributes "+
+                                               "is 255 characters. Use text attributes "+
+                                               "wherever greater capacity is desired");
+        }
+        if(cache != null && id < cache.length)
+        {
+            cache[(int)id] = str;
+        }
+        Statement stmt = conn.createStatement();
+        checkExists(id, stmt);
+        stmt.execute(
+            "UPDATE "+getTable()+" SET data = '"+
+            escape(str)+
+            "' WHERE data_key = "+id
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void delete(long id, Connection conn)
+        throws EntityDoesNotExistException, SQLException
+    {
+        super.delete(id, conn);
+        if(cache != null && id < cache.length)
+        {
+            cache[(int)id] = null;
+        }
+    }
+    
+    // meta information //////////////////////////////////////////////////////
+    
+    /**
+     * {@inheritDoc}
+     */
+    public boolean supportsExternalString()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getSupportedConditions()
+    {
+        return CONDITION_EQUALITY | CONDITION_COMPARISON | 
+            CONDITION_APPROXIMATION;
+    }
+
+    // protected /////////////////////////////////////////////////////////////
+
+    /**
+     * {@inheritDoc}
+     */
+    public String toExternalString(Object value)
+    {
+        checkValue(value);
+        return "'"+escape((String)value)+"'";
+    }
+}
---
+++ coral-datatypes/src/test/java/org/objectledge/coral/datatypes/IntegerAttributeHandlerTest.java
@@ -0,0 +1,255 @@
+// 
+//Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+//All rights reserved. 
+//   
+//Redistribution and use in source and binary forms, with or without modification,  
+//are permitted provided that the following conditions are met: 
+//   
+//* Redistributions of source code must retain the above copyright notice,  
+//this list of conditions and the following disclaimer. 
+//* Redistributions in binary form must reproduce the above copyright notice,  
+//this list of conditions and the following disclaimer in the documentation  
+//and/or other materials provided with the distribution. 
+//* Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//nor the names of its contributors may be used to endorse or promote products  
+//derived from this software without specific prior written permission. 
+// 
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+//OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+//WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+//POSSIBILITY OF SUCH DAMAGE. 
+//
+package org.objectledge.coral.datatypes;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.jmock.Mock;
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.schema.AttributeClass;
+import org.objectledge.coral.schema.AttributeHandler;
+import org.objectledge.coral.schema.CoralSchema;
+import org.objectledge.coral.security.CoralSecurity;
+import org.objectledge.coral.store.CoralStore;
+import org.objectledge.database.Database;
+import org.objectledge.utils.LedgeTestCase;
+
+/**
+ * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
+ *
+ */
+public class IntegerAttributeHandlerTest extends LedgeTestCase
+{
+    private Mock mockDatabase;
+    private Database database;
+    private Mock mockCoralStore;
+    private CoralStore coralStore;
+    private Mock mockCoralSecurity;
+    private CoralSecurity coralSecurity;
+    private Mock mockCoralSchema;
+    private CoralSchema coralSchema;
+    private Mock mockAttributeClass;
+    private AttributeClass attributeClass;
+
+    private Mock mockConnection;
+    private Connection connection;
+    private Mock mockStatement;
+    private Statement statement;
+    private Mock mockResultSet;
+    private ResultSet resultSet;
+
+    private IntegerAttributeHandler handler;
+
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        mockDatabase = mock(Database.class);
+        mockDatabase.stubs().method("getNextId").will(returnValue(1L));
+        database = (Database)mockDatabase.proxy();
+
+        mockCoralStore = mock(CoralStore.class);
+        coralStore = (CoralStore)mockCoralStore.proxy();
+        mockCoralSchema = mock(CoralSchema.class);
+        coralSchema = (CoralSchema)mockCoralSchema.proxy();
+        mockCoralSecurity = mock(CoralSecurity.class);
+        coralSecurity = (CoralSecurity)mockCoralSecurity.proxy();
+        mockAttributeClass = mock(AttributeClass.class);
+        attributeClass = (AttributeClass)mockAttributeClass.proxy();
+        mockAttributeClass.stubs().method("getJavaClass").will(returnValue(Integer.class));
+        mockAttributeClass.stubs().method("getName").will(returnValue("long"));
+        mockAttributeClass.stubs().method("getDbTable").will(returnValue("coral_attribute_integer"));
+        handler = new IntegerAttributeHandler(database, coralStore, coralSecurity, coralSchema, attributeClass);
+        mockStatement = mock(Statement.class);
+        statement = (Statement)mockStatement.proxy();
+        mockConnection = mock(Connection.class);
+        mockConnection.stubs().method("createStatement").will(returnValue(statement));
+        connection = (Connection)mockConnection.proxy();
+        mockResultSet = mock(ResultSet.class);
+        resultSet = (ResultSet)mockResultSet.proxy();
+        mockStatement.stubs().method("executeQuery").will(returnValue(resultSet));
+        mockResultSet.stubs().method("close").isVoid();
+        mockStatement.stubs().method("close").isVoid();
+    }
+
+    public void testAttributeHandlerBase()
+    {
+        assertNotNull(handler);
+    }
+
+    public void testCreate() throws Exception
+    {
+        String stmt = "INSERT INTO " + "coral_attribute_integer" + "(data_key, data) VALUES (1, 1000)";
+        mockStatement.expects(once()).method("execute").with(eq(stmt)).will(returnValue(true));
+        handler.create(new Integer(1000), connection);
+        String stmt2 = "INSERT INTO coral_attribute_integer(data_key, data) VALUES (1, 1000)";
+        mockStatement.expects(once()).method("execute").with(eq(stmt2)).will(returnValue(true));
+        handler.create(new Integer(1000), connection);
+    }
+
+    public void testUpdate() throws Exception
+    {
+        mockResultSet.expects(once()).method("next").will(returnValue(true));
+        String stmt2 = "UPDATE coral_attribute_integer SET data = 1000 WHERE data_key = 1";
+        mockStatement.expects(once()).method("execute").with(eq(stmt2)).will(returnValue(true));
+        handler.update(1,new Integer(1000),connection);
+        mockResultSet.expects(once()).method("next").will(returnValue(false));
+        try
+        {
+            handler.update(1,new Integer(1000),connection);
+            fail("should throw the excpetion");
+        }
+        catch(EntityDoesNotExistException e)
+        {
+            //ok!
+        }
+    }
+
+    public void testRetrieve() throws Exception
+    {
+        mockResultSet.expects(once()).method("next").will(returnValue(true));
+        mockResultSet.expects(once()).method("getInt").will(returnValue(1000));
+        handler.retrieve(1, connection);
+        mockResultSet.expects(once()).method("next").will(returnValue(false));
+        try
+        {
+            handler.retrieve(1, connection);
+            fail("should throw the exception");
+        }
+        catch (EntityDoesNotExistException e)
+        {
+            //ok!
+        }
+    }
+
+    public void testGetSupportedConditions()
+    {
+        assertEquals(AttributeHandler.CONDITION_EQUALITY | 
+                     AttributeHandler.CONDITION_COMPARISON,
+                     handler.getSupportedConditions());
+    }
+
+    public void testGetComparator()
+    {
+        Object comp = handler.getComparator();
+        assertNotNull(comp);
+    }
+
+    public void testSupportsExternalString()
+    {
+        assertEquals(true, handler.supportsExternalString());
+    }
+
+    public void testToAttributeValue()
+    {
+        assertEquals(new Integer(1000), handler.toAttributeValue("1000"));
+        try
+        {
+            handler.toAttributeValue("foo");
+            fail("should throw the exception");
+        }
+        catch(IllegalArgumentException e)
+        {
+            //ok!
+        }
+    }
+
+    public void testToPrintableString()
+    {
+        assertEquals("1000", handler.toPrintableString(new Integer(1000)));
+    }
+
+    public void testToExternalString()
+    {
+        assertEquals("1000", handler.toExternalString(new Integer(1000)));
+    }
+
+    public void testCheckDomainString()
+    {
+        try
+        {
+            handler.checkDomain("");
+            fail("should throw the exception");
+        }
+        catch (IllegalArgumentException e)
+        {
+            //ok!
+        }
+    }
+
+    public void testCheckDomainStringObject()
+    {
+        try
+        {
+            handler.checkDomain("","");
+            fail("should throw the exception");
+        }
+        catch (IllegalArgumentException e)
+        {
+            //ok!
+        }
+
+    }
+
+    public void testIsComposite()
+    {
+        assertEquals(false, handler.isComposite());
+    }
+
+    public void testContainsResourceReferences()
+    {
+        assertEquals(false, handler.containsResourceReferences());
+    }
+
+    public void testGetResourceReferences()
+    {
+        try
+        {
+            handler.getResourceReferences(null);
+            fail("should throw the exception");
+        }
+        catch(UnsupportedOperationException e)
+        {
+            //ok!
+        }
+    }
+
+    public void testClearResourceReferences()
+    {
+        try
+        {
+            handler.clearResourceReferences(null);
+            fail("should throw the exception");
+        }
+        catch(UnsupportedOperationException e)
+        {
+            //ok!
+        }        
+    }
+}
