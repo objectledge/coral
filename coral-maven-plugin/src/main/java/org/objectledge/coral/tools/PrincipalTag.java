08e640f27b738d31360e055cbede703966693a23
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-06-22 11:30:20 +0000

    release 1.0.3-b3

---
+++ coral-maven-plugin/src/main/java/org/objectledge/coral/tools/PrincipalTag.java
@@ -0,0 +1,97 @@
+// 
+// Copyright (c) 2003,2004 , Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.coral.tools;
+
+import java.security.Principal;
+
+import org.apache.commons.jelly.JellyTagException;
+import org.apache.commons.jelly.MissingAttributeException;
+import org.apache.commons.jelly.XMLOutput;
+
+/**
+ * A tag for instatntiating java.security.Principal objects in a Jelly script.
+ *
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: PrincipalTag.java,v 1.5 2005-02-16 16:20:50 rafal Exp $
+ */
+public class PrincipalTag
+    extends CoralPluginTag
+{
+    private String variable;
+    
+    private String name;
+    
+    /**
+     * Sets the name.
+     *
+     * @param name The name to set.
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    /**
+     * Sets the var.
+     *
+     * @param var The var to set.
+     */
+    public void setVariable(String var)
+    {
+        this.variable = var;
+    }
+    
+    /**
+     * Retruns Principal instance with the specified name.
+     * 
+     * @return Principal instance with the specified name.
+     * @throws MissingAttributeException if the name attrbiute has not been set.
+     */
+    public Principal getPrincipal()
+        throws MissingAttributeException
+    {
+        checkAttribute(name, "name");
+        return new Principal()
+        {
+            public String getName()
+            {
+                return name;
+            }
+        };
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void doTag(XMLOutput out) 
+        throws MissingAttributeException, JellyTagException
+    {
+        checkAttribute(variable, "variable");
+        getContext().setVariable(variable, getPrincipal());
+    }
+}
---
+++ coral-maven-plugin/src/test/resources/generator/Custom1.java
@@ -0,0 +1,13 @@
+// license
+package foo;
+
+public class Custom1
+{
+    // generated
+    // @custom
+    // @order a,b,c
+    // @import java.util.Date
+    // @import java.lang.reflect.Method
+    // @extends fred
+    // user defined
+}
