c029cdabd1119d3ae15ec5550d940091e4505ca5
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-12-03 17:18:30 +0000

    regenerated resource wrapper after coral plugin changes

--- coral-web/src/main/java/org/objectledge/coral/table/SymbolicLink.java
+++ coral-web/src/main/java/org/objectledge/coral/table/SymbolicLink.java
@@ -42,7 +42,7 @@ public interface SymbolicLink
 {
     // constants /////////////////////////////////////////////////////////////
 
-    /** The name of the Coral resource class. */    
+    /** The name of the ARL resource class. */    
     public static final String CLASS_NAME = "coral.table.SymbolicLink";
 
     // public interface //////////////////////////////////////////////////////
