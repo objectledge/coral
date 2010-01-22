4ab88bf2b5dfda1c255d10c00792ce837fc1d0f0
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-01-22 14:56:17 +0000

    release 1.0.4

---
+++ coral-web/src/main/java/org/objectledge/coral/util/PermissionSelectionState.java
@@ -0,0 +1,60 @@
+package org.objectledge.coral.util;
+
+import javax.servlet.http.HttpSession;
+
+import org.objectledge.context.Context;
+import org.objectledge.coral.entity.Entity;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.web.HttpContext;
+
+/**
+ * 
+ *
+ * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
+ * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
+ * 
+ * @version $Id: PermissionSelectionState.java,v 1.2 2004-07-14 17:31:06 pablo Exp $
+ */
+public class PermissionSelectionState 
+	extends CoralEntitySelectionState
+{
+    /**
+     * Retrieve the state from session.
+     * 
+     * @param context the context.
+     * @param name the name.
+     * @return the selection state.
+     */
+    public static PermissionSelectionState getAbstractState(Context context, String name)
+    {
+        HttpContext httpContext = HttpContext.getHttpContext(context);
+        HttpSession session = httpContext.getRequest().getSession();
+        PermissionSelectionState currentState = (PermissionSelectionState)
+            session.getAttribute(name);
+        if(currentState == null)
+        {
+            currentState = new PermissionSelectionState(name);
+            session.setAttribute(name, currentState);
+        }
+        return currentState;
+    }
+
+    /**
+     * The constructor.
+     * 
+     * @param name the name of the state.
+     */
+    public PermissionSelectionState(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Entity getEntity(CoralSession coralSession, long id)
+    	throws Exception
+    {
+        return coralSession.getSecurity().getPermission(id);
+    }
+}
