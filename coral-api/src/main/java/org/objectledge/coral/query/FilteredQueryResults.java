48f6c4c94446b6fb1b5e952e323f22a108edbd32
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-01-22 10:08:48 +0000

    release 1.0.4-b1

---
+++ coral-api/src/main/java/org/objectledge/coral/query/FilteredQueryResults.java
@@ -0,0 +1,133 @@
+package org.objectledge.coral.query;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.objectledge.coral.schema.AttributeClass;
+
+/**
+ * Represents the results of a query as tuples of Resource properties.
+ *
+ * @author <a href="mailto:rkrzewsk@ngo.pl">Rafal Krzewski</a>
+ * @version $Id: FilteredQueryResults.java,v 1.4 2006-04-26 11:10:07 rafal Exp $
+ */
+public interface FilteredQueryResults
+    extends Iterable<FilteredQueryResults.Row>
+{
+    // iteration over results ////////////////////////////////////////////////
+
+    /**
+     * Returns an Iterator over a list of {@link FilteredQueryResults.Row} objects.
+     *
+     * <p>The iterator does not support <code>remove</code> operation.
+     *
+     * @return an Iterator over a list of {@link FilteredQueryResults.Row} objects.
+     */
+    public Iterator<Row> iterator();
+
+    /**
+     * Returns a list of {@link FilteredQueryResults.Row} objects.
+     *
+     * <p>This methods iterates over the whole result set and collects all the
+     * rows in an ArrayList for subsequent random access.</p>
+     *
+     * @return a list of {@link FilteredQueryResults.Row} objects.
+     */
+    public List<Row> getList();
+
+    // results metadata //////////////////////////////////////////////////////
+
+    /**
+     * Returns the total number of columns in the results.
+     *
+     * <p>Note that columns are numbered 1 .. columnCount inclusive, just like
+     * in <code>java.sql.ResultSet</code>.</p>
+     * 
+     * @return the  total number of columns in the results.
+     */
+    public int getColumnCount();
+    
+    /**
+     * Returns the name of the specified result column.
+     *
+     * @param index the index of the column (1 based).
+     * @return the name of the specified column.
+     * @throws IndexOutOfBoundsException if the index if the index is out of
+     *         1..columnCount range.
+     */
+    public String getColumnName(int index)
+        throws IndexOutOfBoundsException;
+    
+    /**
+     * Returns the index of the specified result column.
+     *
+     * @param name the name of the column.
+     * @return the index of the specified column.
+     * @throws IllegalArgumentException if no column by the specified name is
+     *         present in the results.
+     */
+    public int getColumnIndex(String name)
+        throws IllegalArgumentException;
+
+    /**
+     * Returns the type of attribute in the specified column.
+     *
+     * @param index the index of the column (1 based).
+     * @return the type of the specified column.
+     * @throws IndexOutOfBoundsException if the index if the index is out of
+     *         1..columnCount range.
+     */
+    public AttributeClass<?> getColumnType(int index)
+        throws IndexOutOfBoundsException;    
+
+    // nested inteface ///////////////////////////////////////////////////////
+    
+    /**
+     * Represens a single query result.
+     */
+    public interface Row
+    {
+        /**
+         * Returns the selected column of the row.
+         *
+         * @param name the name of the column.
+         * @return the value.
+         * @throws IllegalArgumentException if no column by the specified name is
+         *         present in the results.
+         */
+        public Object get(String name)
+            throws IllegalArgumentException;
+        
+        /**
+         * Returns the selected column of the row.
+         *
+         * @param index the index of the column (1 based).
+         * @return the value.
+         * @throws IndexOutOfBoundsException if the index if the index is out of
+         *         1..columnCount range.
+         */
+        public Object get(int index)
+            throws IndexOutOfBoundsException;
+
+        /**
+         * Returns the value from the only column of the row.
+         *
+         * @return the value.
+         * @throws IllegalStateException if there is more than one column in
+         *         the query results.
+         */
+        public Object get()
+            throws IllegalStateException;
+
+        /**
+         * Returns the contents of the row as array.
+         *
+         * <p>The indices in the array are 0 based, on contrary to the general
+         * indexing of QueryResults columns. Thus the valid indexes are 0
+         * .. columnCount-1 inclusive.</p>
+         * 
+         * @return the current row contents.
+         */
+        public Object[] getArray();
+    }
+}
---
+++ coral-api/src/main/java/org/objectledge/coral/security/CoralRoleChecking.java
@@ -0,0 +1,89 @@
+// 
+// Copyright (c) 2003-2005, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.coral.security;
+
+import java.security.Principal;
+
+import org.objectledge.authentication.UserUnknownException;
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.session.CoralSessionFactory;
+import org.objectledge.security.RoleChecking;
+
+/**
+ * 
+ *
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: CoralRoleChecking.java,v 1.1 2005-05-18 06:53:04 pablo Exp $
+ */
+public class CoralRoleChecking
+    implements RoleChecking
+{
+    private final CoralSessionFactory coralSessionFactory;
+
+    /**
+     * Creates new CoralRoleChecking instance.
+     * 
+     * @param coralSessionFactory the coral session factory.
+     */
+    public CoralRoleChecking(CoralSessionFactory coralSessionFactory)
+    {
+        this.coralSessionFactory = coralSessionFactory;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public String[] getRoles(Principal user)
+        throws UserUnknownException
+    {
+        CoralSession current = coralSessionFactory.getCurrentSession();
+        if(current == null)
+        {
+            throw new IllegalStateException("no Coral session is active. "+
+                "CoralSessionInitValve must precede PolicyCheckingValve in the pipeline");
+        }
+        Subject subject;
+        try
+        {
+            subject = current.getSecurity().getSubject(user.getName());
+        }
+        catch(EntityDoesNotExistException e)
+        {
+            throw new UserUnknownException("unknown user "+user.getName());
+        }
+        Role[] roles = subject.getRoles();
+        String[] result = new String[roles.length];
+        int i = 0;
+        for(Role r : roles)
+        {
+            result[i++] = r.getName();
+        }
+        return result;
+    }
+}
