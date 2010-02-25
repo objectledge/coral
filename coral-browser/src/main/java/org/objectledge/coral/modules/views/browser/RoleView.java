3114bcb85b9c12358458cca3ff8dc35e70699895
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-02-25 16:16:52 +0000

    tagging 1.0.5 release

---
+++ coral-browser/src/main/java/org/objectledge/coral/modules/views/browser/RoleView.java
@@ -0,0 +1,44 @@
+package org.objectledge.coral.modules.views.browser;
+
+import org.jcontainer.dna.Logger;
+import org.objectledge.context.Context;
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.security.Role;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.i18n.I18nContext;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.table.TableStateManager;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.mvc.MVCContext;
+import org.objectledge.web.mvc.security.PolicySystem;
+
+/**
+ * The role view screen.
+ */
+public class RoleView extends BaseBrowserView
+{
+    public RoleView(Context context, PolicySystem policySystemArg, Logger logger, TableStateManager tableStateManager)
+    {
+        super(context, policySystemArg, logger, tableStateManager);
+    }
+    
+    public void process(Parameters parameters, TemplatingContext templatingContext, 
+        MVCContext mvcContext, I18nContext i18nContext, CoralSession coralSession)
+        throws ProcessingException
+    {
+        try
+        {
+            long roleId = parameters.getLong("role_id",-1);
+            if (roleId != -1)
+            {
+                Role role = coralSession.getSecurity().getRole(roleId);
+                templatingContext.put("role", role);
+            }
+        }
+        catch (EntityDoesNotExistException e)
+        {
+            throw new ProcessingException("Role not found", e);
+        }
+    }
+}
