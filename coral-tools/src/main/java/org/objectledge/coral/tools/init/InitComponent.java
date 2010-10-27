eac050a88892c03f240a9b3994105f01b2b04b43
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-10-27 18:31:52 +0000

    release 1.0.7

---
+++ coral-tools/src/main/java/org/objectledge/coral/tools/init/InitComponent.java
@@ -0,0 +1,110 @@
+package org.objectledge.coral.tools.init;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import javax.sql.DataSource;
+
+import org.objectledge.database.DatabaseUtils;
+import org.objectledge.filesystem.FileSystem;
+
+/**
+ * A component that initializes a database schema for use as Coral store.
+ * 
+ * @author rafal
+ */
+public class InitComponent
+{
+    /** DataSource for accessing the database. */
+    private final DataSource dataSource;
+    
+    /** Ledge file system for accessing Coral sql scripts on the classpath. */
+    private final FileSystem fileSystem;
+    
+    /**
+     * Creates a new instance of the component.
+     * 
+     * @param dataSource DataSource for accessing the database.
+     * @param fileSystem Ledge file system for accessing Coral sql scripts on the classpath.
+     */
+    public InitComponent(DataSource dataSource, FileSystem fileSystem)
+    {
+        this.dataSource = dataSource;
+        this.fileSystem = fileSystem;
+    }
+    
+    /**
+     * Initializes the database for coral use.
+     * 
+     * @throws Exception if the initialization failed.
+     */
+    public void run()
+        throws Exception
+    {
+        if(!hasTable("ledge_id_table"))
+        {
+            runScript("sql/database/IdGeneratorTables.sql");
+        }
+        else
+        {
+            runScript("sql/database/IdGeneratorCleanup.sql");
+        }
+        
+        if(!hasTable("ledge_parameters"))
+        {
+            runScript("sql/parameters/db/DBParametersTables.sql");
+        }
+        else
+        {
+            runScript("sql/parameters/db/DBParametersCleanup.sql");
+        }
+        
+        if(!hasTable("ledge_scheduler"))
+        {
+            runScript("sql/scheduler/db/DBSchedulerTables.sql");
+        }
+        else
+        {
+            runScript("sql/scheduler/db/DBSchedulerCleanup.sql");
+        }
+        
+        if(!hasTable("ledge_naming_attribute"))
+        {
+            runScript("sql/naming/db/DBNamingTables.sql");
+        }
+        else
+        {
+            runScript("sql/naming/db/DBNamingCleanup.sql");
+        }
+        
+        if(!hasTable("coral_resource_class"))
+        {
+            runScript("sql/coral/CoralRITables.sql");
+            runScript("sql/coral/CoralDatatypesTables.sql");
+        }
+        else
+        {
+            runScript("sql/coral/CoralDatatypesCleanup.sql");
+            runScript("sql/coral/CoralRICleanup.sql");
+        }
+        runScript("sql/coral/CoralRIInitial.sql");
+        runScript("sql/coral/CoralDatatypesInitial.sql");
+    }
+    
+    private boolean hasTable(String table)
+        throws Exception
+    {
+        return DatabaseUtils.hasTable(dataSource, table);
+    }
+    
+    private void runScript(String path)
+        throws Exception
+    {
+        Reader scriptReader = fileSystem.getReader(path, "UTF-8");
+        if(scriptReader == null)
+        {
+            throw new IOException("script " + path + " missing from classpath");
+        }
+        DatabaseUtils.runScript(dataSource, scriptReader);
+    }
+}
