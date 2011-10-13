144ada468268aff01ed0ce67c585042679af3e13
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2011-10-13 10:37:03 +0000

    rolling back release 1.0.11.2

---
+++ coral-ri/src/main/java/org/objectledge/coral/schema/AttributeClassImpl.java
@@ -0,0 +1,292 @@
+package org.objectledge.coral.schema;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectledge.coral.BackendException;
+import org.objectledge.coral.InstantiationException;
+import org.objectledge.coral.Instantiator;
+import org.objectledge.coral.entity.AbstractEntity;
+import org.objectledge.coral.event.AttributeClassChangeListener;
+import org.objectledge.coral.event.CoralEventHub;
+import org.objectledge.database.persistence.InputRecord;
+import org.objectledge.database.persistence.OutputRecord;
+import org.objectledge.database.persistence.Persistence;
+import org.objectledge.database.persistence.PersistenceException;
+
+/**
+ * Represents an attribute type.
+ *
+ * @version $Id: AttributeClassImpl.java,v 1.11 2004-03-16 13:33:35 fil Exp $
+ * @author <a href="mailto:rkrzewsk@ngo.pl">Rafal Krzewski</a>
+ */
+public class AttributeClassImpl
+    extends AbstractEntity
+    implements AttributeClass,
+               AttributeClassChangeListener
+{
+    // Instance variables ///////////////////////////////////////////////////////////////////////
+    
+    /** Instantiator. */
+    private Instantiator instantiator;
+    
+    /** The CoralEventHub. */
+    private CoralEventHub coralEventHub;
+    
+    /** name of the Java class. */
+    private String javaClassName;
+    
+    /** The associated Java class. */
+    private Class javaClass;
+
+    /** name of the handler class. */
+    private String handlerClassName;
+    
+    /** The associated {@link org.objectledge.coral.schema.AttributeHandler}. */
+    private AttributeHandler handler;
+
+    /** The associated database table. */
+    private String dbTable;
+    
+    // Initialization ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Constructs a {@link AttributeClassImpl}.
+     *
+     * @param persistence the Peristence subsystem.
+     * @param instantiator the Instantiator.
+     * @param coralEventHub the CoralEventHub.
+     */
+    public AttributeClassImpl(Persistence persistence, Instantiator instantiator, 
+        CoralEventHub coralEventHub)
+    {
+        super(persistence);
+        this.instantiator = instantiator;
+        this.coralEventHub = coralEventHub;
+    }
+
+    /**
+     * Constructs a {@link AttributeClassImpl}.
+     *
+     * @param persistence the Peristence subsystem.
+     * @param instantiator the Instantiator.
+     * @param coralEventHub the CoralEventHub.
+     * 
+     * @param name the name of the attribute class.
+     * @param javaClass the name of the Java class associated with this
+     *        attribute class.
+     * @param handlerClass the name of the AttributeHandler implementation
+     *        responsible for this attribute class.
+     * @param dbTable the name of the database table that holds the data of
+     *        attributes belonging to this class.
+     */
+    AttributeClassImpl(Persistence persistence, Instantiator instantiator, 
+        CoralEventHub coralEventHub, 
+        String name, String javaClass, String handlerClass, String dbTable)
+        throws JavaClassException
+    {
+        super(persistence, name);
+        this.instantiator = instantiator;
+        this.coralEventHub = coralEventHub;
+        setDbTable(dbTable);
+        setJavaClass(javaClass);
+        setHandlerClass(handlerClass);
+        coralEventHub.getInbound().addAttributeClassChangeListener(this, this);
+    }
+
+    // Persistent interface /////////////////////////////////////////////////////////////////////
+
+    /** The key columns. */
+    private static final String[] KEY_COLUMNS = { "attribute_class_id" };
+
+    /**
+     * Returns the name of the table this type is mapped to.
+     *
+     * @return the name of the table.
+     */
+    public String getTable()
+    {
+        return "coral_attribute_class";
+    }
+    
+    /** 
+     * Returns the names of the key columns.
+     *
+     * @return the names of the key columns.
+     */
+    public String[] getKeyColumns()
+    {
+        return KEY_COLUMNS;
+    }           
+
+    /**
+     * Stores the fields of the object into the specified record.
+     *
+     * <p>You need to call <code>getData</code> of your superclasses if they
+     * are <code>Persistent</code>.</p>
+     *
+     * @param record the record to store state into.
+     * @throws PersistenceException if there is a problem storing field values.
+     */
+    public void getData(OutputRecord record)
+        throws PersistenceException
+    {
+        super.getData(record);
+        record.setString("db_table_name", dbTable);
+        record.setString("java_class_name", javaClassName);
+        record.setString("handler_class_name", handlerClassName);
+    }
+
+    /**
+     * Loads the fields of the object from the specified record.
+     *
+     * <p>You need to call <code>setData</code> of your superclasses if they
+     * are <code>Persistent</code>.</p>
+     * 
+     * @param record the record to read state from.
+     * @throws PersistenceException if there is a problem loading field values.
+     */
+    public void setData(InputRecord record)
+        throws PersistenceException
+    {
+        super.setData(record);
+        setDbTable(record.getString("db_table_name"));
+        try
+        {
+            setJavaClass(record.getString("java_class_name"));
+            setHandlerClass(record.getString("handler_class_name"));
+        }
+        catch(JavaClassException e)
+        {
+            throw new BackendException("Failed to load AttributeClass #"+id, e);
+        }
+        coralEventHub.getInbound().addAttributeClassChangeListener(this, this);
+    }
+    
+    // AttributeClassChangeListener interface ///////////////////////////////////////////////////
+
+    /**
+     * Called when <code>AttributeClass</code>'s data change.
+     *
+     * @param attributeClass the attributeClass that changed.
+     */
+    public void attributeClassChanged(AttributeClass attributeClass)
+    {
+        if(this.equals(attributeClass))
+        {
+            try
+            {
+                persistence.revert(this);
+            }
+            catch(PersistenceException e)
+            {
+                throw new BackendException("failed to revert entity state", e);
+            }
+        }
+    }
+
+    // AttributeClass interface /////////////////////////////////////////////////////////////////
+
+    /**
+     * Returns the Java class that is associated with this resource attribute
+     * type.
+     *
+     * @return the Java class that is associated with this resource attribute
+     * type.
+     */
+    public Class getJavaClass()
+    {
+        return javaClass;
+    }
+
+    /**
+     * Returns the AttributeHandler implementaion that will manage the
+     * attributes of that class.
+     *
+     * @return an <code>AttributeHandler</code> implementation.
+     */
+    public AttributeHandler getHandler()
+    {
+        return handler;
+    }
+
+    /**
+     * Return the name of a database table that holds the values of the
+     * attributes of that type.
+     *
+     * @return the name of a database table that holds the values of the
+     * attributes of that type.
+     */
+    public String getDbTable()
+    {
+        return dbTable;
+    }
+    
+    // Package private setter methods ///////////////////////////////////////////////////////////
+
+    /**
+     * Sets the Java class that is associated with this resource attribute
+     * type.
+     *
+     * @param className the Java class that is associated with this resource
+     * attribute type.
+     */
+    void setJavaClass(String className)
+        throws JavaClassException
+    {
+        javaClassName = className;
+        try
+        {
+            javaClass = instantiator.loadClass(className);
+        }
+        catch(ClassNotFoundException e)
+        {
+            throw new JavaClassException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Sets the AttributeHandler implementaion that will manage the attributes
+     * of that class. 
+     *
+     * @param className the name of an <code>AttributeHandler</code>
+     * implementation. 
+     */
+    void setHandlerClass(String className)
+        throws JavaClassException
+    {
+        handlerClassName = className;
+        try
+        {
+            Class handlerClass = instantiator.loadClass(className);
+            Map additional = new HashMap();
+            additional.put(AttributeClass.class, this);
+            handler = (AttributeHandler)instantiator.newInstance(handlerClass, additional);
+        }
+        catch(ClassNotFoundException e)
+        {
+            throw new JavaClassException(e.getMessage(), e);
+        }
+        catch(InstantiationException e)
+        {
+            throw new JavaClassException(e.getMessage(), e.getCause());
+        }
+        catch(ClassCastException e)
+        {
+            throw new JavaClassException(className+" does not implement "+
+                                         "AttributeHandler interface", e);
+        }
+    }
+
+    /**
+     * Sets the name of a database table that holds the values of the
+     * attributes of that type.
+     *
+     * @param dbTable the name of a database table that holds the values of the
+     * attributes of that type.
+     */
+    void setDbTable(String dbTable)
+    {
+        this.dbTable = dbTable;
+    }
+}
---
+++ coral-ri/src/main/resources/sql/coral/CoralRICleanup.sql
@@ -0,0 +1,67 @@
+-- 
+-- Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+-- All rights reserved. 
+-- 
+-- Redistribution and use in source and binary forms, with or without modification,  
+-- are permitted provided that the following conditions are met: 
+-- 
+-- * Redistributions of source code must retain the above copyright notice,  
+--   this list of conditions and the following disclaimer. 
+-- * Redistributions in binary form must reproduce the above copyright notice,  
+--   this list of conditions and the following disclaimer in the documentation  
+--   and/or other materials provided with the distribution. 
+-- * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+--   nor the names of its contributors may be used to endorse or promote products  
+--   derived from this software without specific prior written permission. 
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+-- IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+-- INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+-- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+-- OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+-- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+-- POSSIBILITY OF SUCH DAMAGE. 
+-- 
+
+--
+-- Coral RI : cleanup
+--
+-- $Id$
+--
+
+-- cleanup ------------------------------------------------------------------
+
+DELETE FROM coral_relation_data;
+DELETE FROM coral_relation;
+
+DELETE FROM coral_permission_assignment;
+DELETE FROM coral_permission_association;
+DELETE FROM coral_role_assignment;
+DELETE FROM coral_role_implication;
+DELETE FROM coral_attribute_definition;
+DELETE FROM coral_resource_class_inheritance;
+
+-- MySQL: instead of DROP/ADD CONSTRAINT use
+-- SET FOREIGN_KEY_CHECKS=0/1
+
+ALTER TABLE coral_resource DROP CONSTRAINT resource_parent_fk;
+
+DELETE FROM coral_resource;
+
+ALTER TABLE coral_resource ADD CONSTRAINT resource_parent_fk 
+  FOREIGN KEY (parent) REFERENCES coral_resource (resource_id);
+
+DELETE FROM coral_role;
+DELETE FROM coral_subject;
+DELETE FROM coral_permission;
+DELETE FROM coral_attribute_class;
+DELETE FROM coral_resource_class;
+
+DELETE FROM ledge_id_table WHERE table_name = 'coral_relation';
+DELETE FROM ledge_id_table WHERE table_name = 'coral_role';
+DELETE FROM ledge_id_table WHERE table_name = 'coral_subject';
+DELETE FROM ledge_id_table WHERE table_name = 'coral_permission';
+
