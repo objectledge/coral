c5e641e0716d09e08855e2ef8a938319f4baa909
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2011-10-13 10:19:15 +0000

    release 1.0.11.2

--- coral-browser/src/main/webapp/content/script/Browser.js
+++
@@ -1,9 +0,0 @@
-scriptLoader.loadCommon('WinPopup.js');
-scriptLoader.loadCommon('Forms.js');
-scriptLoader.loadCommon('PropertySelector.js');
-
-function selectEntity(attribute, form, element, baseLink)
-{
-  window.propertySelector = new PropertySelector(attribute, form, element);
-  getWinPopup('Coral Browser').open(baseLink, 800, 400, 'center middle');
-}
