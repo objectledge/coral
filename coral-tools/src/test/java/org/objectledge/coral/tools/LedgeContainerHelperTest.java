704dfa43edd5a79a2ecd75fd54218f7097559d2d
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2012-03-03 13:04:22 +0000

    release 1.0.12.1

---
+++ coral-tools/src/test/java/org/objectledge/coral/tools/LedgeContainerHelperTest.java
@@ -0,0 +1,17 @@
+package org.objectledge.coral.tools;
+
+import junit.framework.TestCase;
+
+import org.picocontainer.MutablePicoContainer;
+
+public class LedgeContainerHelperTest
+    extends TestCase
+{
+    public void testSimpleContainer()
+        throws Exception
+    {
+        MutablePicoContainer ledgeContainer = LedgeContainerFactory.newLedgeContainer(
+            "src/test/resources/container", "simple", null);
+        assertNotNull(ledgeContainer);
+    }
+}
