0257dc842e367cb8dd76f2a80c0566544315b4cc
Rafał Krzewski <rafal.krzewski@objectledege.org>
2012-08-16 19:36:57 +0200

    spell out constaraint names

--- coral-datatypes/src/main/resources/sql/coral/CoralDatatypesConstraints.sql
+++ coral-datatypes/src/main/resources/sql/coral/CoralDatatypesConstraints.sql
@@ -1,47 +1,47 @@
 -- generic resource ---------------------------------------------------------
 
 ALTER TABLE coral_generic_resource 
-        ADD FOREIGN KEY (attribute_definition_id)
+        ADD CONSTRAINT coral_generic_resource_attribute_definition_id_fkey FOREIGN KEY (attribute_definition_id)
         REFERENCES coral_attribute_definition (attribute_definition_id);
 
 ALTER TABLE coral_generic_resource
-        ADD FOREIGN KEY (resource_id)
+        ADD CONSTRAINT coral_generic_resource_resource_id_fkey FOREIGN KEY (resource_id)
         REFERENCES coral_resource (resource_id);
 
 -- resource class attribute
 
 ALTER TABLE coral_attribute_resource_class 
-        ADD FOREIGN KEY (ref)
+        ADD CONSTRAINT coral_attribute_resource_class_ref_fkey FOREIGN KEY (ref)
         REFERENCES coral_resource_class (resource_class_id);
 
 -- resource attribute
 
 ALTER TABLE coral_attribute_resource 
-        ADD FOREIGN KEY (ref)
+        ADD CONSTRAINT coral_attribute_resource_ref_fkey FOREIGN KEY (ref)
         REFERENCES coral_resource (resource_id);
 
 -- subject attribute 
         
 ALTER TABLE coral_attribute_subject 
-        ADD FOREIGN KEY (ref)
+        ADD CONSTRAINT coral_attribute_subject_ref_fkey FOREIGN KEY (ref)
         REFERENCES coral_subject (subject_id);
 
 -- role attribute
         
 ALTER TABLE coral_attribute_role 
-        ADD FOREIGN KEY (ref)
+        ADD CONSTRAINT coral_attribute_role_ref_fkey FOREIGN KEY (ref)
         REFERENCES coral_role (role_id);
 
 -- permission attribute        
 
 ALTER TABLE coral_attribute_permission 
-        ADD FOREIGN KEY (ref)
+        ADD CONSTRAINT coral_attribute_permission_ref_fkey FOREIGN KEY (ref)
         REFERENCES coral_permission (permission_id);
 
 -- resource list attribute
 
 ALTER TABLE coral_attribute_resource_list
-        ADD FOREIGN KEY (ref)
+        ADD CONSTRAINT coral_attribute_resource_list_ref_fkey FOREIGN KEY (ref)
         REFERENCES coral_resource (resource_id);
 
 
--- coral-ri/src/main/resources/sql/coral/CoralRIConstraints.sql
+++ coral-ri/src/main/resources/sql/coral/CoralRIConstraints.sql
@@ -1,80 +1,80 @@
 -- resource class inheritance -----------------------------------------------
 
 ALTER TABLE coral_resource_class_inheritance 
-        ADD FOREIGN KEY (parent)
+        ADD CONSTRAINT coral_resource_class_inheritance_parent_fkey FOREIGN KEY (parent)
         REFERENCES coral_resource_class (resource_class_id);
 
 ALTER TABLE coral_resource_class_inheritance
-        ADD FOREIGN KEY (child)
+        ADD CONSTRAINT coral_resource_class_inheritance_child_fkey FOREIGN KEY (child)
         REFERENCES coral_resource_class (resource_class_id);
 
 -- attribute definition -----------------------------------------------------
 
 ALTER TABLE coral_attribute_definition 
-        ADD FOREIGN KEY (resource_class_id)
+        ADD CONSTRAINT coral_attribute_definition_resource_class_id_fkey FOREIGN KEY (resource_class_id)
         REFERENCES coral_resource_class (resource_class_id);
 
 ALTER TABLE coral_attribute_definition
-        ADD FOREIGN KEY (attribute_class_id)
+        ADD CONSTRAINT coral_attribute_definition_attribute_class_id_fkey FOREIGN KEY (attribute_class_id)
         REFERENCES coral_attribute_class (attribute_class_id);
 
 -- role implication ---------------------------------------------------------
 
 ALTER TABLE coral_role_implication 
-        ADD FOREIGN KEY (super_role)
+        ADD CONSTRAINT coral_role_implication_super_role_fkey FOREIGN KEY (super_role)
         REFERENCES coral_role (role_id);
 
 ALTER TABLE coral_role_implication
-        ADD FOREIGN KEY (sub_role)
+        ADD CONSTRAINT coral_role_implication_sub_role_fkey FOREIGN KEY (sub_role)
         REFERENCES coral_role (role_id);
 
 -- role assignment ----------------------------------------------------------
 
 ALTER TABLE coral_role_assignment
-        ADD FOREIGN KEY (subject_id)
+        ADD CONSTRAINT coral_role_assignment_subject_id_fkey FOREIGN KEY (subject_id)
         REFERENCES coral_subject(subject_id);
 
 ALTER TABLE coral_role_assignment
-        ADD FOREIGN KEY (role_id)
+        ADD CONSTRAINT coral_role_assignment_role_id_fkey FOREIGN KEY (role_id)
         REFERENCES coral_role(role_id);
 
 ALTER TABLE coral_role_assignment
-        ADD FOREIGN KEY (grantor)
+        ADD CONSTRAINT coral_role_assignment_grantor_fkey FOREIGN KEY (grantor)
         REFERENCES coral_subject(subject_id);
 
 -- resource -----------------------------------------------------------------
 
 ALTER TABLE coral_resource
-        ADD FOREIGN KEY (resource_class_id)
+        ADD CONSTRAINT coral_resource_resource_class_id_fkey FOREIGN KEY (resource_class_id)
         REFERENCES coral_resource_class (resource_class_id);
 
 ALTER TABLE coral_resource
-        ADD FOREIGN KEY (parent)
+        ADD CONSTRAINT coral_resource_parent_fkey FOREIGN KEY (parent)
         REFERENCES coral_resource (resource_id);
 
 ALTER TABLE coral_resource
-        ADD FOREIGN KEY (created_by)
+        ADD CONSTRAINT coral_resource_created_by_fkey FOREIGN KEY (created_by)
         REFERENCES coral_subject (subject_id);
 
 ALTER TABLE coral_resource
-        ADD FOREIGN KEY (owned_by)
+        ADD CONSTRAINT coral_resource_owned_by_fkey FOREIGN KEY (owned_by)
         REFERENCES coral_subject (subject_id);
 
 ALTER TABLE coral_resource
-        ADD FOREIGN KEY (modified_by)
+        ADD CONSTRAINT coral_resource_modified_by_fkey FOREIGN KEY (modified_by)
         REFERENCES coral_subject (subject_id);
 
 -- relation -----------------------------------------------------------------
 
 ALTER TABLE coral_relation_data
-        ADD FOREIGN KEY (relation_id)
+        ADD CONSTRAINT coral_relation_data_relation_id_fkey FOREIGN KEY (relation_id)
         REFERENCES coral_relation (relation_id);
 
 ALTER TABLE coral_relation_data
-        ADD FOREIGN KEY (resource1)
+        ADD CONSTRAINT coral_relation_data_resource1_fkey FOREIGN KEY (resource1)
         REFERENCES coral_resource (resource_id);
 
 ALTER TABLE coral_relation_data
-        ADD FOREIGN KEY (resource2)
+        ADD CONSTRAINT coral_relation_data_resource2_fkey FOREIGN KEY (resource2)
         REFERENCES coral_resource (resource_id);
 
