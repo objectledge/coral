01828e01fbd8b90871a9841c1553a60f86a6b2ab
Rafał Krzewski <rafal.krzewski@objectledege.org>
2012-10-26 14:52:21 +0200

    LEDGE-202: Replaced JOTM/XAPool with Bitronix

--- coral-maven2-plugin/src/main/java/org/objectledege/coral/tools/maven/RmlRunnerMojo.java
+++ coral-maven2-plugin/src/main/java/org/objectledege/coral/tools/maven/RmlRunnerMojo.java
@@ -75,6 +75,7 @@ public class RmlRunnerMojo
         {
             Map<Object, Object> componentInstances = new HashMap<Object, Object>();
             componentInstances.put(DataSource.class, dataSource);
+            componentInstances.put(Transaction.class, tm);
             MutablePicoContainer container = LedgeContainerFactory.newLedgeContainer(baseDir,
                 configDir, componentInstances);
             fileSystem = (FileSystem)container.getComponentInstance(FileSystem.class);
