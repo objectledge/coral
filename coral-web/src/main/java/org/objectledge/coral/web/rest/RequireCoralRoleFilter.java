dcae5813853dcaf2fb26839bdd998fd90ac6c9ff
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-21 17:06:53 +0100

    BAZYNGO-654 Add filter which runs on annotated methods with @RequireCoralRole.

---
+++ coral-web/src/main/java/org/objectledge/coral/web/rest/RequireCoralRoleFilter.java
@@ -0,0 +1,50 @@
+package org.objectledge.coral.web.rest;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+
+import org.objectledge.coral.security.Role;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.session.CoralSessionFactory;
+
+@RequireCoralRole(role = "thisIsJustRequiredValue")
+public class RequireCoralRoleFilter
+    implements ContainerRequestFilter
+{
+
+    @Inject
+    private CoralSessionFactory coralSessionFactory;
+
+    @Context
+    Provider<ResourceInfo> resourceInfo;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext)
+        throws IOException
+    {
+        final Annotation[] annotations = resourceInfo.get().getResourceMethod().getAnnotations();
+        for(Annotation annotation : annotations)
+        {
+            if(annotation instanceof RequireCoralRole)
+            {
+                RequireCoralRole requireCoralRole = (RequireCoralRole)annotation;
+                final String requiredRole = requireCoralRole.role();
+                final CoralSession currentSession = coralSessionFactory.getCurrentSession();
+                Role coralRole = currentSession.getSecurity().getUniqueRole(requiredRole);
+                if(!currentSession.getUserSubject().hasRole(coralRole))
+                {
+                    throw new WebApplicationException(401);
+                }
+            }
+        }
+    }
+
+}
