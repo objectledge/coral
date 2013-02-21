5f5c0a06ea8d7b6636f712ee597c20b5a67a0fef
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-21 17:07:07 +0100

    BAZYNGO-654 Register filter.

---
+++ coral-web/src/main/java/org/objectledge/coral/web/rest/RegisterCoralFilters.java
@@ -0,0 +1,16 @@
+package org.objectledge.coral.web.rest;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.objectledge.web.rest.JerseyConfigurationHook;
+
+public class RegisterCoralFilters
+    implements JerseyConfigurationHook
+{
+
+    @Override
+    public void configure(ResourceConfig config)
+    {
+        config.register(RequireCoralRoleFilter.class);
+    }
+
+}
