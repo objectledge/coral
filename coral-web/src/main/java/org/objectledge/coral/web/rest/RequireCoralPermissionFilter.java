d084db78cb5d89ad797dd18252c9462bc48e904d
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-22 11:14:12 +0100

    BAZYNGO-654 Rewrite filter after changing annotation design.

--- coral-web/src/main/java/org/objectledge/coral/web/rest/RequireCoralPermissionFilter.java
+++ coral-web/src/main/java/org/objectledge/coral/web/rest/RequireCoralPermissionFilter.java
@@ -5,20 +5,17 @@ import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Provider;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.objectledge.coral.entity.EntityDoesNotExistException;
@@ -28,6 +25,10 @@ import org.objectledge.coral.session.CoralSession;
 import org.objectledge.coral.session.CoralSessionFactory;
 import org.objectledge.coral.store.Resource;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+
+@RequireCoralPermission(permission = "*")
 public class RequireCoralPermissionFilter
     implements ContainerRequestFilter
 {
@@ -46,91 +47,119 @@ public class RequireCoralPermissionFilter
     public void filter(ContainerRequestContext requestContext)
         throws IOException
     {
-        final Annotation[][] parameterAnnotations = resourceInfo.get().getResourceMethod()
-            .getParameterAnnotations();
-        for(Annotation[] annotationsForParameter : parameterAnnotations)
+        final Annotation[] annotations = resourceInfo.get().getResourceMethod().getAnnotations();
+        Collection<RequireCoralPermission> requireCoralPermissions = findRequireCoralPermissions(annotations);
+
+        try(CoralSession session = coralSessionFactory.getCurrentSession())
         {
-            Collection<String> permissions = null;
-            String queryParamName = null;
-            String pathParamName = null;
-            for(Annotation annotation : annotationsForParameter)
+            for(RequireCoralPermission requireCoralPermission : requireCoralPermissions)
             {
-                if(annotation instanceof RequireCoralPermission)
+                validate(requireCoralPermission);
+                Permission permission = getPermission(requireCoralPermission.permission(), session);
+                final Collection<Long> resourceIds = getResourceId(requireCoralPermission);
+                Collection<Resource> resources = getResources(resourceIds, session);
+                final Subject user = session.getUserSubject();
+                for(Resource resource : resources)
                 {
-                    if(permissions == null)
+                    if(!user.hasPermission(resource, permission))
                     {
-                        permissions = new ArrayList<>();
+                        throw new WebApplicationException(401);
                     }
-                    RequireCoralPermission requireCoralPermission = (RequireCoralPermission)annotation;
-                    permissions.add(requireCoralPermission.value());
-                }
-                else if(annotation instanceof QueryParam)
-                {
-                    queryParamName = QueryParam.class.cast(annotation).value();
-                }
-                else if(annotation instanceof PathParam)
-                {
-                    pathParamName = QueryParam.class.cast(annotation).value();
-                }
-            }
-            if((queryParamName == null && permissions != null && pathParamName == null))
-            {
-                throw new RuntimeException("Misuse of annotations");
-            }
-            Collection<String> resources = null;
-            if(queryParamName != null && permissions != null)
-            {
-                final List<String> resourceIds = uriInfo.get().getQueryParameters()
-                    .get(queryParamName);
-                resources = resourceIds;
-            }
-            else if(pathParamName != null && permissions != null)
-            {
-                final String resourceId = uriInfo.get().getPathParameters().getFirst(pathParamName);
-                resources = Arrays.asList(resourceId);
-            }
-
-            if(resources != null)
-            {
-                final CoralSession session = coralSessionFactory.getCurrentSession();
-                final Set<Permission> coralPermissions = getCoralPermissions(permissions, session);
-                final Subject userSubject = session.getUserSubject();
-                for(String resourceId : resources)
-                {
-                    checkPermissions(resourceId, coralPermissions, userSubject, session);
-                }
-            }
-        }
-    }
-
-    private Set<Permission> getCoralPermissions(Collection<String> permissions, CoralSession session)
-    {
-        Set<Permission> coralPermissions = new HashSet<>();
-        for(String permission : permissions)
-        {
-            coralPermissions.addAll(Arrays.asList(session.getSecurity().getPermission(permission)));
-        }
-        return coralPermissions;
-    }
-
-    private void checkPermissions(String resourceId, Collection<Permission> permissions,
-        Subject user, CoralSession session)
-    {
-        try
-        {
-            final Resource resource = session.getStore().getResource(Long.valueOf(resourceId));
-            for(Permission permission : permissions)
-            {
-                if(!user.hasPermission(resource, permission))
-                {
-                    throw new WebApplicationException(401);
                 }
             }
         }
         catch(EntityDoesNotExistException e)
         {
-            throw new WebApplicationException(Response.status(404).entity(e).build());
+            throw new WebApplicationException(404);
         }
     }
 
+    private Collection<Resource> getResources(Collection<Long> resourceIds, CoralSession session)
+        throws EntityDoesNotExistException
+    {
+        Collection<Resource> resources = new ArrayList<>();
+        for(Long resourceId : resourceIds)
+        {
+            resources.add(session.getStore().getResource(resourceId.longValue()));
+        }
+        return resources;
+    }
+
+    private Collection<Long> getResourceId(RequireCoralPermission requireCoralPermission)
+    {
+        if(requireCoralPermission.queryParam().equals(""))
+        {
+            // find resource using id from path
+            return Arrays.asList(Long.valueOf(uriInfo.get().getPathParameters()
+                .getFirst(requireCoralPermission.pathParam())));
+        }
+        else
+        {
+            // find resource using id from query parameters
+            final List<String> ids = uriInfo.get().getQueryParameters()
+                .get(requireCoralPermission.queryParam());
+            return Collections2.transform(ids, new Function<String, Long>()
+                {
+                    @Override
+                    @Nullable
+                    public Long apply(@Nullable String resourceId)
+                    {
+                        return Long.valueOf(resourceId);
+                    }
+                });
+        }
+    }
+
+    private Permission getPermission(String permission, CoralSession session)
+    {
+        return session.getSecurity().getUniquePermission(permission);
+    }
+
+    /**
+     * Checks if annotation was used correctly. Throws RuntimeException if misused
+     * 
+     * @param requireCoralPermission
+     */
+    private void validate(RequireCoralPermission requireCoralPermission)
+    {
+        if((!requireCoralPermission.pathParam().equals("id"))
+            && (!requireCoralPermission.queryParam().equals("")))
+        {
+            // both pathParam and queryParam were specified which is wrong
+            throw new RuntimeException(
+                "RequireCoralPermission annotation was misused. Check javadoc for correct usage");
+        }
+    }
+
+    /**
+     * Finds RequireCoralPermissions
+     * 
+     * @param annotations from method
+     * @return collection with annotations or empty list, never null
+     */
+    private Collection<RequireCoralPermission> findRequireCoralPermissions(Annotation[] annotations)
+    {
+        Collection<RequireCoralPermission> requireCoralPermissions = null;
+        for(Annotation annotation : annotations)
+        {
+            if(annotation instanceof RequireCoralPermission)
+            {
+                final RequireCoralPermission requireCoralPermission = RequireCoralPermission.class
+                    .cast(annotation);
+                if(requireCoralPermissions == null)
+                {
+                    requireCoralPermissions = new ArrayList<>();
+                }
+                requireCoralPermissions.add(requireCoralPermission);
+            }
+        }
+        if(requireCoralPermissions != null)
+        {
+            return requireCoralPermissions;
+        }
+        else
+        {
+            return Collections.emptyList();
+        }
+    }
 }
