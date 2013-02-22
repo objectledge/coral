89b4c292dbf6aa0150f4f43d6a147aec05a86e88
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-22 11:13:47 +0100

    BAZYNGO-654 Change annotation behaviour. It is explained in javadoc.

--- coral-web/src/main/java/org/objectledge/coral/web/rest/RequireCoralPermission.java
+++ coral-web/src/main/java/org/objectledge/coral/web/rest/RequireCoralPermission.java
@@ -1,6 +1,6 @@
 package org.objectledge.coral.web.rest;
 
-import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 
 import java.lang.annotation.Retention;
@@ -8,10 +8,39 @@ import java.lang.annotation.Target;
 
 import javax.ws.rs.NameBinding;
 
+/**
+ * Annotation to mark method with required permissions.
+ * 
+ * <pre>
+ * Examples:
+ * {@code
+ *  @RequireCoralPermission(permission="some.permission") 
+ * 
+ * Checks permission "some.permission" on resource which Id is taken from Path parameter named "id"
+ * }
+ * {@code
+ *  @RequireCoralPermission(permission="some.permission", pathParam="pathParamName")
+ * }
+ * Checks permission "some.permission" on resource which Id is taken from Path parameter named "pathParamName"
+ * 
+ * {@code
+ *  @RequireCoralPermission(permission="some.permission", queryParam="queryParamName")
+ * } 
+ *  Checks permission "some.permission" on resource which Id is taken from Query parameter named "queryParamName"
+ * 
+ * </pre>
+ * 
+ * @author Marek Lewandowski
+ * @see CoralResourceId
+ */
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@Target({ PARAMETER, TYPE })
+@Target({ TYPE, METHOD })
 @NameBinding
 public @interface RequireCoralPermission
 {
-    String value();
+    String permission();
+
+    String pathParam() default "id";
+
+    String queryParam() default "";
 }
