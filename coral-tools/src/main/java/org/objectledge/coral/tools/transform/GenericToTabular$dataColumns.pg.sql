947a633eace1b7c167c1e0e1b5f1256c48129166
Rafał Krzewski <rafal.krzewski@objectledege.org>
2013-02-23 14:32:07 +0100

    CORAL-127: create target tables and transfer data for resource classes that already use tabular format (PersistentResourceHandler)

--- coral-tools/src/main/java/org/objectledge/coral/tools/transform/GenericToTabular$dataColumns.pg.sql
+++ coral-tools/src/main/java/org/objectledge/coral/tools/transform/GenericToTabular$dataColumns.pg.sql
@@ -6,9 +6,7 @@ from
   coral_resource_class rc, 
   coral_attribute_definition ad, 
   coral_attribute_mapping am 
-where 
-  rc.handler_class_name = 'org.objectledge.coral.datatypes.GenericResourceHandler' 
-  and rc.flags & 4 = 0 -- ignore builtin classes
+where rc.flags & 4 = 0 -- ignore builtin classes
   and ad.resource_class_id = rc.resource_class_id
   and ad.attribute_class_id = am.attribute_class_id
 order by 
