6073b8644012caca2f5cfe618cc8039e3d98ff8c
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-03-04 17:26:16 +0100

    BAZYNGO-720 Add interface for security processors

---
+++ coral-web/src/main/java/org/objectledge/coral/web/rest/security/SecurityViolationException.java
@@ -0,0 +1,17 @@
+package org.objectledge.coral.web.rest.security;
+
+public class SecurityViolationException
+    extends Exception
+{
+
+    public SecurityViolationException(String string)
+    {
+        super(string);
+    }
+
+    public SecurityViolationException()
+    {
+
+    }
+
+}
